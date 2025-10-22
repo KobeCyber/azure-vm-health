3b66ac1871d733c67ff77546f49d25b1cd0b3dde
root <root@4877aa50082c.mylabserver.com>
2025-10-22 05:02:10 +0000

    lots of doing :)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,20 @@
 resource "azurerm_resource_group" "monitoring" {
-name = "Monitoring_Boss"
-location = "East US"
+  name     = "Monitoring_Boss"
+  location = "East US"
 }
-resource "azurerm_virtual_network" "vnet" {}
-resource "azurerm_subnet" "subnet" {}
-resource "azurerm_network_security_group" "_nsg" {}
-resource "azurerm_network_interface" "nic" {}
-resource "azurerm_linux_virtual_machine" "vm" {}
-resource "azurerm_storage_account" "storage" {}
-resource "azurerm_log_analytics_workspace" "log_a" {}
-resource "azurerm_monitor_metric_alert" "alert" {}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "monitorin-vnet"
+  location            = azurerm_resource_group.monitoring.location
+  resource_group_name = azurerm_resource_group.monitoring.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["1.1.1.1", "8.8.8.8"]
+
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "monitoring-subnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.monitoring.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
