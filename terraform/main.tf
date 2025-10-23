21939572c7af17af27be9f287b1dfdd0b892a12c
root <root@4877aa50082c.mylabserver.com>
2025-10-23 23:34:37 +0000

    quick update

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "vnet" {
 #Subnet
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "monitoring-subne"t
+  name                 = "monitoring-subnet"
   address_prefixes     = ["10.0.1.0/24"]
   resource_group_name  = var.rg
   virtual_network_name = var.vnet
