86aadd5812aa5d98c5c12edc8f4fac5235479631
root <root@4877aa50082c.mylabserver.com>
2025-10-23 19:05:35 +0000

    renaming nsg resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
 
-resource "azurerm_network_security_group" "_nsg" {
+resource "azurerm_network_security_group" "nsg" {
 name = "monitoring-nsg"
 resource_group_name = azurerm_resource_group.monitoring.name
 
