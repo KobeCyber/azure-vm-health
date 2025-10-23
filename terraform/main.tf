54f6fb8f16dcb2fb46e6aa8ca0175f9e701bbd00
root <root@4877aa50082c.mylabserver.com>
2025-10-23 19:39:20 +0000

    Adding simple varaibles

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "monitoring" {
   name     = "Monitoring_Boss"
-  location = "East US"
+  location = var.region
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "monitoring-vnet"
-  location            = azurerm_resource_group.monitoring.location
-  resource_group_name = azurerm_resource_group.monitoring.name
+  location            = var.region
+  resource_group_name = var.rg
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["1.1.1.1", "8.8.8.8"]
 
@@ -15,12 +15,29 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "monitoring-subnet"
   address_prefixes     = ["10.0.1.0/24"]
-  resource_group_name  = azurerm_resource_group.monitoring.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.rg
+  virtual_network_name = var.vnet
 }
 
 resource "azurerm_network_security_group" "nsg" {
 name = "monitoring-nsg"
-resource_group_name = azurerm_resource_group.monitoring.name
+resource_group_name = var.rg
 
 }
+
+resource "azurerm_network_security_group" "monitoring-ssh" {
+  name                = "ssh-enabled"
+  location            = azurerm_resource_group.montioring.name
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  description = "The Azure region to deploy resources in"
+  type        = string
+  default     = "East US"
+}
+
+variable "rg" {
+  description = "Name of the resource group for monitoring"
+  type        = string
+  default     = "Monitoring_Boss"
+}
+
+variable "vnet" {
+  description = "Name of the virtual network"
+  type        = string
+  default     = "monitoring-vnet"
+}
+
 
