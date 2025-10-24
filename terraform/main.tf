fc8e4de56e1aaf92f05182c48432e32f23b15e43
root <root@4877aa50082c.mylabserver.com>
2025-10-24 15:11:53 +0000

    Fixing Linux VM image not being found

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,11 +84,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
